4889e0b5537612e1175b7cc3a54f7b4cee1503bb
andrecharland <andre.charland@gmail.com>
2019-10-22 15:51:32 -0700

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,8 +7,8 @@ exports.handler = async function Index () {
     /**
      * Basic bio
      */
-    fullname: 'Your Name', // ←  Start by adding your name!
-    title: 'My personal site!',
+    fullname: 'Andre Charland', // ←  Start by adding your name!
+    title: 'My incredible home page!',
     occupation: 'Artist & Photographer',
     location: 'West Glacier, MT',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
